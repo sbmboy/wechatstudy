ceba83f63f42358c87d7181ea4e2dde353cce8d5
sbmboy <sbmboy@gmail.com>
2018-07-29 13:30:07 +0800

    条件渲染，模板渲染示例

---
+++ helloworld/pages/5.template/index.js
@@ -0,0 +1,14 @@
+// pages/5.template/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    item:{
+      name:"朱海龙",
+      phone:"13661969651",
+      address:"松江区，莘砖公路668号",
+    }
+  }
+})
